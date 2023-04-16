205fc2cfeb95da0fd969a07728185805259a92f6
solis <solis@DESKTOP-96R3VE0>
2023-04-17 01:18:35 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace LibrarySystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtauthor = new System.Windows.Forms.TextBox();
+            this.txttitle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtno = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.grid1 = new System.Windows.Forms.DataGridView();
+            this.accessionnumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.authorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.libSysDataSet = new LibrarySystem.LibSysDataSet();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.bookTableAdapter = new LibrarySystem.LibSysDataSetTableAdapters.bookTableAdapter();
+            this.masterDataSet = new LibrarySystem.masterDataSet();
+            this.bookBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.bookTableAdapter1 = new LibrarySystem.masterDataSetTableAdapters.bookTableAdapter();
+            this.masterDataSet1 = new LibrarySystem.masterDataSet1();
+            this.bookBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.bookTableAdapter2 = new LibrarySystem.masterDataSet1TableAdapters.bookTableAdapter();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libSysDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(249, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Library System";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnEdit);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtauthor);
+            this.groupBox1.Controls.Add(this.txttitle);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtno);
+            this.groupBox1.Location = new System.Drawing.Point(33, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(697, 124);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fill Here";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(583, 85);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(583, 53);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(583, 19);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtauthor
+            // 
+            this.txtauthor.Location = new System.Drawing.Point(117, 82);
+            this.txtauthor.Name = "txtauthor";
+            this.txtauthor.Size = new System.Drawing.Size(405, 20);
+            this.txtauthor.TabIndex = 0;
+            // 
+            // txttitle
+            // 
+            this.txttitle.Location = new System.Drawing.Point(117, 56);
+            this.txttitle.Name = "txttitle";
+            this.txttitle.Size = new System.Drawing.Size(405, 20);
+            this.txttitle.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Author";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Accession Number";
+            // 
+            // txtno
+            // 
+            this.txtno.Location = new System.Drawing.Point(117, 30);
+            this.txtno.Name = "txtno";
+            this.txtno.Size = new System.Drawing.Size(405, 20);
+            this.txtno.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grid1);
+            this.groupBox2.Controls.Add(this.txtSearch);
+            this.groupBox2.Location = new System.Drawing.Point(33, 230);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(697, 202);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search / View";
+            // 
+            // grid1
+            // 
+            this.grid1.AutoGenerateColumns = false;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.accessionnumberDataGridViewTextBoxColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.authorDataGridViewTextBoxColumn});
+            this.grid1.DataSource = this.bookBindingSource;
+            this.grid1.Location = new System.Drawing.Point(6, 45);
+            this.grid1.Name = "grid1";
+            this.grid1.Size = new System.Drawing.Size(674, 150);
+            this.grid1.TabIndex = 1;
+            // 
+            // accessionnumberDataGridViewTextBoxColumn
+            // 
+            this.accessionnumberDataGridViewTextBoxColumn.DataPropertyName = "accession_number";
+            this.accessionnumberDataGridViewTextBoxColumn.HeaderText = "accession_number";
+            this.accessionnumberDataGridViewTextBoxColumn.Name = "accessionnumberDataGridViewTextBoxColumn";
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            // 
+            // authorDataGridViewTextBoxColumn
+            // 
+            this.authorDataGridViewTextBoxColumn.DataPropertyName = "author";
+            this.authorDataGridViewTextBoxColumn.HeaderText = "author";
+            this.authorDataGridViewTextBoxColumn.Name = "authorDataGridViewTextBoxColumn";
+            // 
+            // bookBindingSource
+            // 
+            this.bookBindingSource.DataMember = "book";
+            this.bookBindingSource.DataSource = this.libSysDataSet;
+            // 
+            // libSysDataSet
+            // 
+            this.libSysDataSet.DataSetName = "LibSysDataSet";
+            this.libSysDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(6, 19);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(405, 20);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            this.txtSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSearch_KeyPress);
+            // 
+            // bookTableAdapter
+            // 
+            this.bookTableAdapter.ClearBeforeFill = true;
+            // 
+            // masterDataSet
+            // 
+            this.masterDataSet.DataSetName = "masterDataSet";
+            this.masterDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bookBindingSource1
+            // 
+            this.bookBindingSource1.DataMember = "book";
+            this.bookBindingSource1.DataSource = this.masterDataSet;
+            // 
+            // bookTableAdapter1
+            // 
+            this.bookTableAdapter1.ClearBeforeFill = true;
+            // 
+            // masterDataSet1
+            // 
+            this.masterDataSet1.DataSetName = "masterDataSet1";
+            this.masterDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bookBindingSource2
+            // 
+            this.bookBindingSource2.DataMember = "book";
+            this.bookBindingSource2.DataSource = this.masterDataSet1;
+            // 
+            // bookTableAdapter2
+            // 
+            this.bookTableAdapter2.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 464);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Library System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libSysDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtauthor;
+        private System.Windows.Forms.TextBox txttitle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtno;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView grid1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private LibSysDataSet libSysDataSet;
+        private System.Windows.Forms.BindingSource bookBindingSource;
+        private LibSysDataSetTableAdapters.bookTableAdapter bookTableAdapter;
+        private masterDataSet masterDataSet;
+        private System.Windows.Forms.BindingSource bookBindingSource1;
+        private masterDataSetTableAdapters.bookTableAdapter bookTableAdapter1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn accessionnumberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn authorDataGridViewTextBoxColumn;
+        private masterDataSet1 masterDataSet1;
+        private System.Windows.Forms.BindingSource bookBindingSource2;
+        private masterDataSet1TableAdapters.bookTableAdapter bookTableAdapter2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibrarySystem
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection;
+        private SqlDataAdapter adapter;
+        private DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+            // Create a connection to the database
+            connection = new SqlConnection("Data Source=DESKTOP-96R3VE0\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True");
+
+            // Create a new SqlDataAdapter with a SELECT command that retrieves all rows from your table, and a DELETE command that deletes rows from the database
+            adapter = new SqlDataAdapter("SELECT * FROM book", connection);
+            SqlCommand deleteCommand = new SqlCommand("DELETE FROM book WHERE accession_number=@accession_number", connection);
+            deleteCommand.Parameters.Add("@accession_number", SqlDbType.VarChar, 50, "accession_number");
+            adapter.DeleteCommand = deleteCommand;
+
+            // Create a new DataTable to hold the data from the database
+            table = new DataTable();
+
+            // Fill the DataTable with the data from the database
+            adapter.Fill(table);
+
+            // Set the DataGridView control's DataSource property to the DataTable
+            grid1.DataSource = table;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'masterDataSet.book' table. You can move, or remove it, as needed.
+            this.bookTableAdapter1.Fill(this.masterDataSet.book);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Create a new row in the DataTable and set its values
+            DataRow row = table.NewRow();
+            row["accession_number"] = txtno.Text;
+            row["title"] = txttitle.Text;
+            row["author"] = txtauthor.Text;
+
+            // Add the new row to the DataTable
+            table.Rows.Add(row);
+
+            // Update the database with the changes
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Update(table);
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            // Get the selected row from the DataGridView
+            DataGridViewRow selectedRow = grid1.SelectedRows[0];
+
+            // Get the index of the selected row in the DataTable
+            int index = table.Rows.IndexOf((selectedRow.DataBoundItem as DataRowView).Row);
+
+            // Update the values of the selected row in the DataTable
+            table.Rows[index]["accession_number"] = txtno.Text;
+            table.Rows[index]["title"] = txttitle.Text;
+            table.Rows[index]["author"] = txtauthor.Text;
+
+            // Update the database with the changes
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Update(table);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Get the selected row from the DataGridView
+            DataGridViewRow selectedRow = grid1.SelectedRows[0];
+
+            // Get the index of the selected row in the DataTable
+            int index = table.Rows.IndexOf((selectedRow.DataBoundItem as DataRowView).Row);
+
+            // Delete the selected row from the DataTable
+            table.Rows[index].Delete();
+
+            // Update the database with the changes
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Update(table);
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                // Get the search term from the txtSearch TextBox
+                string searchTerm = txtSearch.Text;
+
+                // Create a DataView object to filter the table
+                DataView dataView = new DataView(table);
+                dataView.RowFilter = string.Format("CONVERT(accession_number, 'System.String') LIKE '%{0}%' OR title LIKE '%{0}%' OR author LIKE '%{0}%'", searchTerm);
+
+                // Set the DataGridView control's DataSource property to the filtered DataView
+                grid1.DataSource = dataView;
+            }
+        }
+    }
+}
